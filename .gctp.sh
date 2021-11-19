f8f5cf5853740d6061d0f83eb01ce8bba805ac53
Ivan Imbert <ivan.imbert@epita.fr>
2021-11-19 20:07:10 +0100

    Update .gctp.sh

--- .gctp.sh
+++ .gctp.sh
@@ -1,17 +1,8 @@
 
 gctp() { 
-    echo $#
-    if [[ "$#"!="1" ]]; then
+    if [ "$#" -ne 1 ]; then
         echo "not a possible tp"
     else
-        echo "test"
-        local string="git clone ivan.imbert@git.cri.epita.fr:p/2025-s3-tp/tp0"
-        echo "${string}"
-        
-        string+="$1"
-        echo "$string"
-    
-        string+="-ivan.imbert"
-        echo "$string"
+        git clone "ivan.imbert@git.cri.epita.fr:p/2025-s3-tp/tp$1-ivan.imbert"
     fi
 }
