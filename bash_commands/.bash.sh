1c44cf892c4f1d6a1998be8dd1650aac357d2976
Ivan Imbert <ivan.imbert@epita.fr>
2021-11-20 20:00:05 +0100

    improved functions
    filtered the accepted args of gctp
    cleaned the cdto function using case

--- bash_commands/.bash.sh
+++ bash_commands/.bash.sh
@@ -5,28 +5,39 @@ alias go="explorer.exe ."
 cdto() {
     if [ "$#" -eq "0" ] ; then
         echo "No param"
-    elif [ "$1" = "prog" ] ; then
-        #echo "Moving to Prog"
-        cd "/mnt/g/Data Ivan/cours/épita/Prog/"
-    elif [ "$1" = "algo" ] ; then
-        #echo "Moving to Algo"
-        cd "/mnt/g/Data Ivan/cours/épita/Algo/"
-    elif [ "$1" = "tp" ] ; then
-        #echo "Moving to tp"
-        cd "/mnt/g/Data Ivan/cours/épita/Prog/C/"
-    elif [ "$1" = "code" ] ; then
-        #echo "Moving to code"
-        cd "/mnt/g/Data Ivan/code/"
-    elif [ "$1" = "c" ] ; then
-        #echo "Moving to code/c"
-        cd "/mnt/g/Data Ivan/code/C/"
-    elif [ "$1" = "data" ] ; then
-        #echo "Moving to Data Ivan"
-        cd "/mnt/g/Data Ivan/"
-    elif [ "$1" = "ocr" ] ; then
-        #echo "Moving to OCR"
-        cd "/mnt/g/Data Ivan/cours/épita/Prog/Project S3 Sudoku OCR/OCR_C/"
     else
-        echo "not a known path : '$path'"
+        case $1 in
+            "prog")
+                #echo "Moving to Prog"
+                cd "/mnt/g/Data Ivan/cours/épita/Prog/"
+                ;;
+            "algo")
+                #echo "Moving to Algo"
+                cd "/mnt/g/Data Ivan/cours/épita/Algo/"
+                ;;
+            "tp")
+                #echo "Moving to tp"
+                cd "/mnt/g/Data Ivan/cours/épita/Prog/C/"
+                ;;
+            "code")
+                #echo "Moving to code"
+                cd "/mnt/g/Data Ivan/code/"
+                ;;
+            "c")
+                #echo "Moving to code/c"
+                cd "/mnt/g/Data Ivan/code/C/"
+                ;;
+            "data")
+                #echo "Moving to Data Ivan"
+                cd "/mnt/g/Data Ivan/"
+                ;;
+            "ocr")
+                #echo "Moving to OCR"
+                cd "/mnt/g/Data Ivan/cours/épita/Prog/Project S3 Sudoku OCR/OCR_C/"
+                ;;
+            *)
+                echo "not a known path : '$path'"
+                ;;
+        esac
     fi
 }
--- bash_commands/.git.sh
+++ bash_commands/.git.sh
@@ -1,10 +1,12 @@
 # Create a command to only add modified files
 alias gitadd="git ls-files --modified | xargs git add"
 
+
+
 # A command to git clone the tp with the given number (to update when changing
 # the source path)
 gctp() { 
-    if [ "$#" -ne 1 ]; then
+    if [ "$#" -ne 1 ] || [ "${#1}" -ne 2 ]; then
         echo "not a possible tp"
     else
         git clone "ivan.imbert@git.cri.epita.fr:p/2025-s3-tp/tp$1-ivan.imbert"
