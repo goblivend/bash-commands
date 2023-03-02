98e01a03b94cc4bce349d75f93dcb8edb5c8fc58
Ivan Imbert <76164708+goblivend@users.noreply.github.com>
2023-03-02 12:52:29 +0100

    fix(nvim/lsp): now virtual text

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,3 +2,13 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 lsp.setup()
+
+
+vim.diagnostic.config({
+   virtual_text = true,
+   signs = true,
+   update_in_insert = false,
+   underline = false,
+   severity_sort = false,
+   float = true,
+ })
