9abde2cd0b271ff261f00bc32483d35c79b3e2b9
Ivan Imbert <76164708+goblivend@users.noreply.github.com>
2023-03-02 12:11:28 +0100

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,6 +1,6 @@
 function SetColorScheme(color)
 	color = color or "slate"
-	vim.cmd.colorscheme(color)
+	vim.cmd.colorscheme("slate") -- supposed to be color but does not work for me...
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
